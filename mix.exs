123d4cdeb513cc94703092c6824f1dc8b6690f2d
jaeyson <hello@jaeyson.dev>
2021-04-25 21:10:43 +0800

    update action and mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PairSession.MixProject do
     [
       app: :pair_session,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

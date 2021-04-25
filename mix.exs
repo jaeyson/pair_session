7b53391be9ae1543c5fe13d41263359687cc8f1c
jaeyson <hello@jaeyson.dev>
2021-04-25 21:17:04 +0800

    update action to use rc version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PairSession.MixProject do
     [
       app: :pair_session,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
